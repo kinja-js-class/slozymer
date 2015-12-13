4dfbd1a52bd48ec19dc440616d7ed2c8f6b5b24c
Oliver Kovacs <oliver@kinja.com>
2015-12-13 23:16:18 +0100

    data persistance with firebase

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,18 +10,5 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 (function(document) {
 	'use strict';
 
-	var roomList = document.querySelector('room-list');
-
-	// See https://github.com/Polymer/polymer/issues/1381
-	window.addEventListener('WebComponentsReady', function() {
-		roomList.rooms = JSON.parse(JSON.stringify([
-			{type: "toilet", label: "frontend szoba - bal", inuse: false},
-			{type: "toilet", label: "frontend szoba - jobb", inuse: false},
-			{type: "meeting", label: "az egyetlnunk", inuse: true},
-			{type: "toilet", label: "editor szoba - zuhany", inuse: true},
-			{type: "toilet", label: "editor szoba - kicsi", inuse: false},
-			{type: "meeting", label: "a jovobeli", inuse: false}
-		]));
-	});
 
 })(document);
